4a459051079b2b9822134a8135131be93fd1f6d4
Sam <samuel.dacosta@student.manchester.ac.uk>
2017-05-28 23:50:09 +0100

    Do some more

---
+++ src/components/ChatHead.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Chat Head</h1>
+        <input type="text" v-model="message">
+        <p>{{ message }} {{ data.msg }} {{ data.num }} </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["data"],
+        data() {
+
+            return {
+                message: ''
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ChatHead from './components/ChatHead.vue'
+
+Vue.component("chat-head", ChatHead);
 
 new Vue({
   el: '#app',
